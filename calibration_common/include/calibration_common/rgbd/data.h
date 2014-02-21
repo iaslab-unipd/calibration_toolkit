bee2de372146974c986de9e22091782672c586d1
Filippo Basso <bassofil@gmail.com>
2014-02-21 11:29:46 +0100

    Added RGBDData class.

---
+++ calibration_common/include/calibration_common/rgbd/data.h
@@ -0,0 +1,143 @@
+/*
+ *  Copyright (c) 2013-2014, Filippo Basso <bassofil@dei.unipd.it>
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder(s) nor the
+ *        names of its contributors may be used to endorse or promote products
+ *        derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#ifndef CALIBRATION_COMMON_RGBD_DATA_H_
+#define CALIBRATION_COMMON_RGBD_DATA_H_
+
+#include <opencv2/opencv.hpp>
+#include <calibration_common/pinhole/sensor.h>
+#include <calibration_common/depth/sensor.h>
+
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+
+namespace calibration
+{
+
+namespace Types
+{
+
+typedef pcl::PointXYZ PCLPoint;
+typedef pcl::PointXYZRGB PCLPointRGB;
+
+typedef pcl::PointCloud<PCLPoint> PCLCloud;
+typedef pcl::PointCloud<PCLPointRGB> PCLCloudRGB;
+
+} /* namespace Types */
+
+class RGBDData
+{
+public:
+
+  typedef boost::shared_ptr<RGBDData> Ptr;
+  typedef boost::shared_ptr<const RGBDData> ConstPtr;
+
+  RGBDData(int id)
+    : id_(id)
+  {
+    // Do nothing
+  }
+
+  RGBDData(const RGBDData & other)
+    : id_(other.id_),
+      color_data_(other.color_data_.clone()),
+      depth_data_(boost::make_shared<Types::PCLCloud>(*other.depth_data_)),
+      fused_data_(boost::make_shared<Types::PCLCloudRGB>(*other.fused_data_)),
+      color_sensor_(other.color_sensor_),
+      depth_sensor_(other.depth_sensor_)
+  {
+    // Do nothing
+  }
+
+  int id() const
+  {
+    return id_;
+  }
+
+  void setId(int id)
+  {
+    id_ = id;
+  }
+
+  const cv::Mat & colorData() const
+  {
+    return color_data_;
+  }
+
+  void setColorData(const cv::Mat & color_data)
+  {
+    color_data_ = color_data.clone();
+  }
+
+  const Types::PCLCloud::Ptr & depthData() const
+  {
+    return depth_data_;
+  }
+
+  void setDepthData(const Types::PCLCloud & depth_data);
+
+  void fuseData() const;
+
+  Types::PCLCloudRGB::Ptr fusedData() const;
+
+  const PinholeSensor::ConstPtr & colorSensor() const
+  {
+    return color_sensor_;
+  }
+
+  void setColorSensor(const PinholeSensor::ConstPtr & color_sensor)
+  {
+    color_sensor_ = color_sensor;
+  }
+
+  const DepthSensor::ConstPtr & depthSensor() const
+  {
+    return depth_sensor_;
+  }
+
+  void setDepthSensor(const DepthSensor::ConstPtr & depth_sensor)
+  {
+    depth_sensor_ = depth_sensor;
+  }
+
+private:
+
+  int id_;
+
+  PinholeSensor::ConstPtr color_sensor_;
+  cv::Mat color_data_;
+
+  DepthSensor::ConstPtr depth_sensor_;
+  Types::PCLCloud::Ptr depth_data_;
+
+  Types::PCLCloudRGB::Ptr fused_data_;
+
+};
+
+} /* namespace calibration */
+#endif /* CALIBRATION_COMMON_RGBD_DATA_H_ */
---
+++ calibration_common/src/calibration_common/rgbd/data.cpp
@@ -0,0 +1,114 @@
+/*
+ *  Copyright (c) 2013-2014, Filippo Basso <bassofil@dei.unipd.it>
+ *
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *     1. Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *     2. Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *     3. Neither the name of the copyright holder(s) nor the
+ *        names of its contributors may be used to endorse or promote products
+ *        derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <calibration_common/rgbd/data.h>
+#include <calibration_common/base/pcl_conversion.h>
+#include <omp.h>
+
+namespace calibration
+{
+
+void RGBDData::setDepthData(const Types::PCLCloud & depth_data)
+{
+  assert(depth_sensor_);
+  depth_data_ = boost::make_shared<Types::PCLCloud>(depth_data);
+  depth_data_->header.frame_id = depth_sensor_->frameId();
+
+  fused_data_ = boost::make_shared<Types::PCLCloudRGB>();
+  fused_data_->width = depth_data_->width;
+  fused_data_->height = depth_data_->height;
+  fused_data_->is_dense = depth_data_->is_dense;
+  fused_data_->header.frame_id = depth_data_->header.frame_id;
+
+  for (size_t p_index = 0; p_index < depth_data_->points.size(); ++p_index)
+  {
+    const Types::PCLPoint & point = depth_data_->points[p_index];
+
+    Types::PCLPointRGB point_rgb;
+    point_rgb.x = point.x;
+    point_rgb.y = point.y;
+    point_rgb.z = point.z;
+    point_rgb.b = 100;
+    point_rgb.g = 100;
+    point_rgb.r = 100;
+
+    fused_data_->points.push_back(point_rgb);
+  }
+
+}
+
+Types::PCLCloudRGB::Ptr RGBDData::fusedData() const
+{
+  return fused_data_;
+}
+
+void RGBDData::fuseData() const
+{
+  assert(depth_sensor_ and color_sensor_);
+  fused_data_->header.stamp = depth_data_->header.stamp;
+
+  Types::Transform t = color_sensor_->pose().inverse();
+
+  Types::Point3Matrix depth_data_tmp = PCLConversion<Types::Scalar>::toPointMatrix(*depth_data_);
+  depth_sensor_->undistortionModel()->undistort(depth_data_tmp);
+
+#pragma omp parallel for
+  for (size_t i = 0; i < depth_data_tmp.size(); ++i)
+  {
+    Types::PCLPointRGB & point_rgb = fused_data_->points[i];
+    Types::Point3Matrix::Element point_eigen = depth_data_tmp[i];
+
+    point_rgb.x = point_eigen.x();
+    point_rgb.y = point_eigen.y();
+    point_rgb.z = point_eigen.z();
+
+    point_eigen = t * point_eigen;
+    Types::Point2 point_image = color_sensor_->cameraModel()->project3dToPixel(point_eigen);
+
+    int x = static_cast<int>(point_image[0]);
+    int y = static_cast<int>(point_image[1]);
+
+    if (x >= 0 and x < color_data_.cols and y >= 0 and y < color_data_.rows)
+    {
+      cv::Point p(x, y);
+      const cv::Vec3b & image_data = color_data_.at<cv::Vec3b>(p);
+      point_rgb.b = image_data[0];
+      point_rgb.g = image_data[1];
+      point_rgb.r = image_data[2];
+    }
+    else
+    {
+      point_rgb.b = 100;
+      point_rgb.g = 100;
+      point_rgb.r = 100;
+    }
+  }
+
+}
+
+} /* namespace calibration */
