db55152c46c17ab8b873ee67aed54ea16ba8a320
Filippo Basso <bassofil@gmail.com>
2014-04-18 17:18:16 +0200

    Modified ceres_install.sh to work with Ubuntu 14.04 and ROS Indigo.

--- ceres_install.sh
+++ ceres_install.sh
@@ -1,8 +1,7 @@
 # Ceres installation and building following instructions at http://homes.cs.washington.edu/~sagarwal/ceres-solver/stable/building.html
 
 # Initialization
-mkdir /tmp/ceres_install
-mv ceres.patch /tmp/ceres_install
+mkdir -p /tmp/ceres_install
 cd /tmp/ceres_install
 
 # CMake
@@ -28,16 +27,6 @@ sudo make install
 # BLAS & LAPACK
 sudo apt-get install libatlas-base-dev -y
 
-# Install a particular version of Eigen3:
-cd ..
-wget http://bitbucket.org/eigen/eigen/get/3.2.0.tar.bz2
-tar xvjf 3.2.0.tar.bz2
-sudo mv /usr/include/eigen3 /usr/include/eigen3_old
-mkdir eigen_bin
-cd eigen_bin/
-cmake ../eigen-eigen-ffa86ffb5570 -DCMAKE_INSTALL_PREFIX=/usr
-sudo make install
-
 # SuiteSparse and CXSparse (optional)
 # - If you want to build Ceres as a *static* library (the default)
 #   you can use the SuiteSparse package in the main Ubuntu package
@@ -51,9 +40,6 @@ sudo apt-get install libsuitesparse-dev -y
 cd ..
 wget http://ceres-solver.googlecode.com/files/ceres-solver-1.8.0.tar.gz
 tar zxf ceres-solver-1.8.0.tar.gz
-
-# Apply patch
-patch -p 0 -N -r ceres.rej -i ceres.patch
 
 # Install
 mkdir ceres-bin
